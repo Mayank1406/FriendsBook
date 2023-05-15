3e6583b76cd7d9f9a734c840ab1f0a280df00311
Mayank1406 <80544218+Mayank1406@users.noreply.github.com>
2023-05-15 12:05:39 +0530

    #Creating Tables

--- FriendsBook.sql
+++ FriendsBook.sql
@@ -1,4 +1,21 @@
 -- # Creating DataBase bio
 CREATE DATABASE FriendsBook;
 
--- Creating Tables
+-- #Selecting Database
+use Friendsbook;
+
+-- #Creating Tables
+create table register(
+	 user_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+     name varchar(100),
+     surname varchar(100),
+     username varchar(100),
+     email varchar(100),
+     password varchar(100),
+     dateofbirth date,
+     gender varchar(20),
+     city varchar(100),
+     profile_pic varchar(100),
+     date_joined timestamp
+);
+
